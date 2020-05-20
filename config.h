9269568ecdc39b4abac84bab7c4f9e85c4da8137
Orcan Tiryakioglu <orcan.tiryakioglu@gmail.com>
2020-05-20 14:16:51 +0300

    Configure

--- config.h
+++ config.h
@@ -4,16 +4,35 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono Nerd Font:size=11:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* Nord colors */
+#define NORD0 "#2E3440"
+#define NORD1 "#3B4252"
+#define NORD2 "#434C5E"
+#define NORD3 "#4C566A"
+#define NORD4 "#D8DEE9"
+#define NORD5 "#E5E9F0"
+#define NORD6 "#ECEFF4"
+#define NORD7 "#8FBCBB"
+#define NORD8 "#88C0D0"
+#define NORD9 "#81A1C1"
+#define NORD10 "#5E81AC"
+#define NORD11 "#BF616A"
+#define NORD12 "#D08770"
+#define NORD13 "#EBCB8B"
+#define NORD14 "#A3BE8C"
+#define NORD15 "#B48EAD"
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm]          = { NORD6,     NORD0 },
+	[SchemeSel]           = { NORD5,     NORD1 },
+	[SchemeSelHighlight]  = { NORD4,     NORD3 },
+	[SchemeNormHighlight] = { NORD5,     NORD1 },
+	[SchemeOut]           = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
